034570701da0e77e79a8aada0d1e4a8433189c3b
Vishnu <vishnuvasanth26@gmail.com>
2019-11-23 20:54:07 +0530

    updated maven project

---
+++ maven.test/src/main/java/test/app/maven/test/App.java
@@ -0,0 +1,16 @@
+package test.app.maven.test;
+
+import java.awt.image.BufferedImageOp;
+import java.util.Comparator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String s;
+    }
+}
---
+++ maven.test/src/test/java/test/app/maven/test/AppTest.java
@@ -0,0 +1,15 @@
+package test.app.maven.test;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+    public void testApp()
+    {
+    	System.out.println("test");
+    }
+}
